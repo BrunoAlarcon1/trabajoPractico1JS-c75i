c2fc34127cb842673c9c525b8fa2000e72b1793d
bruno.alarcon <alarconbruno14@gmail.com>
2023-10-31 21:34:14 -0300

    agrego ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,8 @@
+const num=parseInt(prompt("Ingrese un numero: "));
+
+if(num % 2 == 0){
+    document.write(`El numero ${num} es divisible por 2`);
+}else{
+    document.write(`El numero ${num} no es divisible por 2`);
+
+}
