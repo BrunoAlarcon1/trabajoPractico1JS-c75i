2a1c95a01a26f7675e021189e5ffaa88364c8cee
bruno.alarcon <alarconbruno14@gmail.com>
2023-10-31 22:23:33 -0300

    agrego ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,11 @@
+const frase = prompt("Ingrese una frase: ");
+let fraseConVocales="";
+
+for(let i=0;i<frase.length;i++){
+    if(frase.charAt(i) == "a" || frase.charAt(i)=="e" || frase.charAt(i)=="i" || frase.charAt(i)=="o" ||frase.charAt(i)=="u"){
+        fraseConVocales=fraseConVocales + frase.charAt(i);
+    }
+
+}
+
+document.write(`<p>${fraseConVocales}</p>`);
