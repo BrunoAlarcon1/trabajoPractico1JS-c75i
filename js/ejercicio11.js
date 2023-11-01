992c6849ebb48b08a13fed0dc47785106bc441a9
bruno.alarcon <alarconbruno14@gmail.com>
2023-10-31 22:49:21 -0300

    agrego ejercicio11

---
+++ js/ejercicio11.js
@@ -0,0 +1,17 @@
+const num= parseInt(prompt("Ingrese un numero: "));
+let mensaje = `El ${num} es divisible `;
+
+if(num%2==0){{
+    mensaje+=`, por 2`;
+}if( num%3==0){
+    mensaje+=`, por 3`;
+
+}if(num%5==0 ){
+    mensaje+=`, por 5`;
+}if(num%7==0){
+    mensaje+=`, por 7`;
+}
+document.write(`<p>${mensaje}</p>`);
+}else{
+    document.write(`<p>El ${num} no es divisible por 2,3,5,7</p>`);
+}
